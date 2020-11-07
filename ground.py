c63d441c68aecb0ece823d8ea4034b1c940f4da3
Timothy Miller <43719088+DruiadinMonk@users.noreply.github.com>
2020-11-07 14:51:18 -0600

    Create ground.py

---
+++ ground.py
@@ -0,0 +1,26 @@
+# Ground class to create top and bottom Ground objects.
+
+
+
+# MODULES
+import pygame
+
+
+
+# LOG CLASS
+class Ground:
+
+
+	# INITIALIZE
+	def __init__(self, window, color, x, y, w, h):
+		self.window = window
+		self.color = color
+		self.x = x
+		self.y = y
+		self.w = w
+		self.h = h
+
+
+	# DRAW FROG
+	def drawGround(self):
+		pygame.draw.rect(self.window, self.color, (self.x, self.y, self.w, self.h))
